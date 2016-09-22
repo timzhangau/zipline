7c3313c0bd7bbf45e0202930ec57dd4bdc4fcb7b
Eddie Hebert <ehebert@quantopian.com>
2016-09-22 11:31:20 -0400

    Save.

--- zipline/api.py
+++ zipline/api.py
@@ -37,6 +37,8 @@ from .utils.events import (
     time_rules
 )
 
+from zipline.assets.futures import ContinuousFuture
+
 __all__ = [
     'EODCancel',
     'FixedSlippage',
@@ -53,5 +55,6 @@ __all__ = [
     'execution',
     'math_utils',
     'slippage',
-    'time_rules'
+    'time_rules',
+    'ContinuousFuture',
 ]
